0347db921ae5ba0029f260e1a3036e2f3676e0ae
unknown <luxassilva@gmail.com>
2023-05-02 05:53:33 -0300

    scroll to top y navbar ya animados

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,11 +1,12 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function card({open2}) {
   return (
-    <article onClick={(e)=>console.log(e.target)} tabIndex={0} className={'flex flex-col p-2 grow cursor-pointer   active:shadow rounded'+(!open2 ?' w-60 h-72':' w-full h-80')}>
-        <img className='select-none pointer-events-none w-full h-full object-cover object-center rounded' src="https://images.unsplash.com/photo-1604866830513-d54766457f45?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="product" />
-        <h5 className='text-[#566270] font-medium'>Monstera Deliciosa</h5>
-        <p>$89</p>
-    </article>
+      <Link rel="noopener noreferrer" href="/" tabIndex={0} onClick={(e)=>console.log(e.target)} className={'flex flex-col p-2 grow cursor-pointer   active:shadow rounded'+(!open2 ?' w-60 h-72':' w-full h-80')}>
+          <img className='select-none pointer-events-none w-full h-full object-cover object-center rounded' src="https://images.unsplash.com/photo-1604866830513-d54766457f45?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="product" />
+          <h2 aria-level="3" className='text-[#566270] font-medium'>Monstera Deliciosa</h2>
+          <p>$89</p>
+      </Link>
   )
 }
--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,7 +1,5 @@
-import Link from 'next/link'
 import React, {useState, useRef} from 'react'
 import Card from './card'
-
 const products = ({scrollRef}) => {
   let [open, setOpen] = useState(false)
   let [open2, setOpen2] = useState(false)
@@ -10,52 +8,60 @@ const products = ({scrollRef}) => {
   let firstOption = useRef()
   let secondOption = useRef()
   const handleOpen = () =>{
-    setOpen(!open)
+      setOpen(!open)
   }
+
   const ArrowChanged = () => (
-    !open ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
-    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-  </svg> : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
-    <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
-  </svg>
-)
-const Select = () => {
-  const selectedFun = (element) => {
-    setSelected(element.textContent)
-    setOpen(!open)
-  }
-  const selectedFunKey = (e, element) => {
-    if(e.key == "Enter"){
+      !open ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+    </svg> : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
+    </svg>
+  )
+
+  const Select = () => {
+    const selectedFun = (element) => {
       setSelected(element.textContent)
       setOpen(!open)
     }
-  }
-    return (
-    <div className="select-none flex gap-2 items-start  w-fit h-fit relative">
-        <button aria-label="Ordenar productos por precio" onClick={()=>handleOpen()} className="select-none relative z-30 bg-white flex justify-center items-center gap-2 shadow p-2 px-4 rounded text-sm text-acento hover:bg-slate-100 transition delay-150 duration-100   ">Sort
-            <ArrowChanged/>
-        </button>
-        <ul role="listbox"   id="my-listbox" aria-expanded={open ? 'true' : 'false'} className={`select-none bg-white w-fit absolute top-10 right-0 z-10 shadow-md border opacity-0 transition-opacity duration-300 rounded  ${open ? "h-auto opacity-100 z-30" : "h-0 hidden"}`}>
-            <li onKeyDown={(e)=>selectedFunKey(e, firstOption.current)} onClick={()=>selectedFun(firstOption.current)} ref={firstOption} aria-selected={selected=="Low Price"} tabIndex="0" aria-controls="my-listbox" role="option" id="my-option-low" className="select-none cursor-pointer px-8 py-2 text-acento text-sm hover:bg-slate-100 min-w-max" >Low Price</li>
-            <li onKeyDown={(e)=>selectedFunKey(e, secondOption.current)} onClick={()=>selectedFun(secondOption.current)} ref={secondOption} aria-selected={selected=="Most Price"} tabIndex="0" aria-controls="my-listbox" role="option" id="my-option-most" className="select-none cursor-pointer px-8 py-2 text-acento text-sm hover:bg-slate-100 min-w-max">Most Price</li>
-        </ul>
-        { open ? <div className="h-screen w-screen fixed z-20 left-0 top-0" onClick={()=>handleOpen()}></div>
-        : ""
-        }
-    </div>
-)
-}
-const SquareGalery = () => {
-  return (
-    <button className='select-none relative z-30 bg-white flex justify-center items-center gap-2 shadow p-2 px-4 rounded text-sm text-acento hover:bg-slate-100 transition delay-150 duration-100' onClick={()=>setOpen2(!open2)}>
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-acento select-none overflow-hidden">
-        <path strokeLinecap="round" strokeLinejoin="round" d={!open2 ? "M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" : "M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z"} />
-      </svg>
-    </button>
+    const selectedFunKey = (e, element) => {
+      if(e.key == "Enter"){
+        setSelected(element.textContent)
+        setOpen(!open)
+      }
+    }
+      return (
+      <div className="select-none flex gap-2 items-start  w-fit h-fit relative">
+          <button aria-label="Ordenar productos por precio" onClick={()=>handleOpen()} className="select-none relative z-30 bg-white flex justify-center items-center gap-2 shadow p-2 px-4 rounded text-sm text-acento hover:bg-slate-100 transition delay-150 duration-100   ">Sort
+              <ArrowChanged/>
+          </button>
+          <ul role="listbox"   id="my-listbox" aria-expanded={open ? 'true' : 'false'} className={`select-none bg-white w-fit absolute top-10 right-0 z-10 shadow-md border opacity-0 transition-opacity duration-300 rounded  ${open ? "h-auto opacity-100 z-30" : "h-0 hidden"}`}>
+              <li onKeyDown={(e)=>selectedFunKey(e, firstOption.current)} onClick={()=>selectedFun(firstOption.current)} ref={firstOption} aria-selected={selected=="Low Price"} tabIndex="0" aria-controls="my-listbox" role="option" id="my-option-low" className="select-none cursor-pointer px-8 py-2 text-acento text-sm hover:bg-slate-100 min-w-max" >Low Price</li>
+              <li onKeyDown={(e)=>selectedFunKey(e, secondOption.current)} onClick={()=>selectedFun(secondOption.current)} ref={secondOption} aria-selected={selected=="Most Price"} tabIndex="0" aria-controls="my-listbox" role="option" id="my-option-most" className="select-none cursor-pointer px-8 py-2 text-acento text-sm hover:bg-slate-100 min-w-max">Most Price</li>
+          </ul>
+          { open ? <div className="h-screen w-screen fixed z-20 left-0 top-0" onClick={()=>handleOpen()}></div>
+          : ""
+          }
+      </div>
   )
-}
+  }
+
+  const handleSelectCategory = (category) => {
+    setCategorySelect(category)
+  }
+
+  const SquareGalery = () => {
+    return (
+      <button className='select-none relative z-30 bg-white flex justify-center items-center gap-2 shadow p-2 px-4 rounded text-sm text-acento hover:bg-slate-100 transition delay-150 duration-100' onClick={()=>setOpen2(!open2)}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-acento select-none overflow-hidden">
+          <path strokeLinecap="round" strokeLinejoin="round" d={!open2 ? "M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" : "M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z"} />
+        </svg>
+      </button>
+    )
+  }
+
   return (
-    <div className="w-full min-h-[100vh] flex flex-col justify-start items-center" ref={scrollRef}>
+    <div className="w-full min-h-[100vh] flex flex-col justify-start items-center py-28" ref={scrollRef}>
         {/* row para sort */}
         <div className="w-full p-4 select-none flex flex-col items-end px-4 gap-4">
           {/* div de sort */}
@@ -69,10 +75,10 @@ const SquareGalery = () => {
               {/* Aside */}
               <aside className="w-[20%] flex flex-col">
                 <ul className='w-full flex flex-col gap-4' >
-                  <li onClick={(e)=>setCategorySelect(e.target.textContent)} className='text-[#566270] cursor-pointer' role="listitem" aria-label="Este elemento seleccionable sirve para indicar que quieres buscar todos los productos" >Todos</li>
-                  <li onClick={(e)=>setCategorySelect(e.target.textContent)} className='text-[#566270] cursor-pointer' role="listitem" aria-label="Este elemento seleccionable sirve para indicar que quieres buscar solo los productos de categoria 'aceites'" >Aceites</li>
-                  <li onClick={(e)=>setCategorySelect(e.target.textContent)} className='text-[#566270] cursor-pointer' role="listitem" aria-label="Este elemento seleccionable sirve para indicar que quieres buscar solo los productos de categoria 'cremas'" >Cremas</li>
-                  <li onClick={(e)=>setCategorySelect(e.target.textContent)} className='text-[#566270] cursor-pointer' role="listitem" aria-label="Este elemento seleccionable sirve para indicar que quieres buscar solo los productos de categoria 'chocolates'" >Chocolates</li>
+                  <li onClick={(e)=>handleSelectCategory(e.target.textContent)} className='text-[#566270] cursor-pointer hover:text-acento' role="listitem" aria-label="Este elemento seleccionable sirve para indicar que quieres buscar todos los productos" >Todos</li>
+                  <li onClick={(e)=>handleSelectCategory(e.target.textContent)} className='text-[#566270] cursor-pointer hover:text-acento' role="listitem" aria-label="Este elemento seleccionable sirve para indicar que quieres buscar solo los productos de categoria 'aceites'" >Aceites</li>
+                  <li onClick={(e)=>handleSelectCategory(e.target.textContent)} className='text-[#566270] cursor-pointer hover:text-acento' role="listitem" aria-label="Este elemento seleccionable sirve para indicar que quieres buscar solo los productos de categoria 'cremas'" >Cremas</li>
+                  <li onClick={(e)=>handleSelectCategory(e.target.textContent)} className='text-[#566270] cursor-pointer hover:text-acento' role="listitem" aria-label="Este elemento seleccionable sirve para indicar que quieres buscar solo los productos de categoria 'chocolates'" >Chocolates</li>
                 </ul>
               </aside>
               {/* products */}
